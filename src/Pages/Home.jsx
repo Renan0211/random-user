2d19a3503490d49f2a4aba2a940ba3ccaa07524d
Renan0211 <renan.antunes.rocha@gmail.com>
2021-11-18 11:53:12 -0300

    começa a criar uma pagina de home

---
+++ src/Pages/Home.jsx
@@ -0,0 +1,14 @@
+import React, { useState, useEffect } from 'react';
+import { Container } from 'react-bootstrap';
+
+const Home = function () {
+  const [usersState, setUsersState] = useState([]);
+  useEffect(() => {
+
+  });
+  return (
+    <Container />
+  );
+};
+
+export default Home;
