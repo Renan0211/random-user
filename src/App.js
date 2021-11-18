948746990f759cf5608ccdd375e70121e7408940
Renan0211 <renan.antunes.rocha@gmail.com>
2021-11-18 11:39:09 -0300

    começa a implementaçao de um novo estilo, alterando o primary padrao do bootstrap

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 /* eslint-disable react/jsx-filename-extension */
 import React from 'react';
-import 'bootstrap/dist/css/bootstrap.min.css';
+import './app.scss';
+// import { Container } from 'react-bootstrap';
 
 const App = function () {
   return (
-    <div className="App">
-      <h1> Random User</h1>
+    <div className="w-100">
+      <div className="bg-primary p-5 rounded-lg m-auto w-100 text-white">
+        <h1 className="text-center"> Random User</h1>
+      </div>
     </div>
   );
 };
