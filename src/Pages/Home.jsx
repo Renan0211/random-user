60e3a19914fac7ad530bed8abede04d891433ad9
Renan0211 <renan.antunes.rocha@gmail.com>
2021-11-18 12:03:37 -0300

    cria na home um estado para armazenar a informaçao sobre os usuarios e chama a a funcao que faz esse fetch no useEffect, assim fazendo o fetch e salvando o resultado quando a pagina e montada

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,11 +1,13 @@
 import React, { useState, useEffect } from 'react';
 import { Container } from 'react-bootstrap';
+import fetchUsers from '../Util/api';
 
 const Home = function () {
   const [usersState, setUsersState] = useState([]);
   useEffect(() => {
-
+    fetchUsers(100).then((data) => setUsersState(data));
   });
+  console.log(usersState);
   return (
     <Container />
   );
