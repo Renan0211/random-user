1d2b59ff08cee22489c882da2d588662e7aaec56
Renan0211 <renan.antunes.rocha@gmail.com>
2021-11-18 13:26:35 -0300

    atualizando de switch para routes, conforme as alteraçoes na v6 do react-router

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 /* eslint-disable react/jsx-filename-extension */
 import React from 'react';
 import './app.scss';
-import { Switch, Route } from 'react-router-dom';
+import { Routes, Route } from 'react-router-dom';
 import Home from './Pages/Home';
 
 const App = function () {
@@ -10,9 +10,9 @@ const App = function () {
       <div className="bg-primary p-5 rounded-lg m-auto w-100 text-white">
         <h1 className="text-center"> Random User</h1>
       </div>
-      <Switch>
-        <Route exact path="/" component={Home} />
-      </Switch>
+      <Routes>
+        <Route exact path="/" element={<Home />} />
+      </Routes>
     </div>
   );
 };
