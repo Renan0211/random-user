1a13a385900912b8741b21d442597ea5f2b5767f
Renan0211 <renan.antunes.rocha@gmail.com>
2021-11-18 11:07:34 -0300

    adiciona um linter no projeto e começa a modificaçao no app.js

--- src/App.js
+++ src/App.js
@@ -1,25 +1,13 @@
-import logo from './logo.svg';
-import './App.css';
+/* eslint-disable react/jsx-filename-extension */
+import React from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
-function App() {
+const App = function () {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1> Random User</h1>
     </div>
   );
-}
+};
 
 export default App;
