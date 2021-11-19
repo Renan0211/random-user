493b157eb7a3ca897c5acaacc79730c3062936ee
Renan0211 <renan.antunes.rocha@gmail.com>
2021-11-19 10:01:06 -0300

    cria um acordeao dentro do offcanvas com informaçao do usuario, por enquanto ele apresenta informacoes de contato e localizacao do usuario

--- src/Components/UserOffCanvas.jsx
+++ src/Components/UserOffCanvas.jsx
@@ -1,12 +1,16 @@
 import React, { useState } from 'react';
 import PropTypes from 'prop-types';
-import { Button, Offcanvas } from 'react-bootstrap';
+import {
+  Button, Container, Offcanvas, Image, Accordion,
+} from 'react-bootstrap';
 
 const UserOffCanvas = function ({ userInfo, placement }) {
   const [show, setShow] = useState(false);
   const handleClose = () => setShow(false);
   const handleShow = () => setShow(true);
-  const { picture, name, gender } = userInfo;
+  const {
+    picture, name, email, phone, cell, location,
+  } = userInfo;
   console.log(picture);
   return (
     <div>
@@ -15,10 +19,30 @@ const UserOffCanvas = function ({ userInfo, placement }) {
       </Button>
       <Offcanvas show={show} onHide={handleClose} placement={placement}>
         <Offcanvas.Header closeButton>
-          <Offcanvas.Title>{`${name.first} ${name.last}`}</Offcanvas.Title>
+          <Offcanvas.Title>{`${name.title} ${name.first} ${name.last}`}</Offcanvas.Title>
         </Offcanvas.Header>
-        <Offcanvas.Body>
-          {gender}
+        <Offcanvas.Body className="d-flex">
+          <Container>
+            <Image src={picture.large} />
+          </Container>
+          <Container>
+            <Accordion>
+              <Accordion.Item eventKey="0">
+                <Accordion.Header>Contato</Accordion.Header>
+                <Accordion.Body>
+                  <p>{`Email: ${email}`}</p>
+                  <p>{`Telefone: ${phone}`}</p>
+                  <p>{`Celular: ${cell}`}</p>
+                </Accordion.Body>
+              </Accordion.Item>
+              <Accordion.Item eventKey="1">
+                <Accordion.Header>Localização</Accordion.Header>
+                <Accordion.Body>
+                  <p>{`${location.city}, ${location.country}`}</p>
+                </Accordion.Body>
+              </Accordion.Item>
+            </Accordion>
+          </Container>
         </Offcanvas.Body>
       </Offcanvas>
     </div>
@@ -37,7 +61,14 @@ UserOffCanvas.propTypes = {
       first: PropTypes.string,
       last: PropTypes.string,
     }),
+    location: PropTypes.shape({
+      city: PropTypes.string,
+      country: PropTypes.string,
+    }),
     gender: PropTypes.string,
+    email: PropTypes.string,
+    phone: PropTypes.string,
+    cell: PropTypes.string,
   }).isRequired,
   placement: PropTypes.string.isRequired,
 };
